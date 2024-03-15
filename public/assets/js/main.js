35fc021448d4a3afe371fe81a80eb33387bb55a2
Gopalakrishnan008 <gopalakrishnan638078@.com>
2024-03-15 18:24:30 +0530

    init: vite config

---
+++ public/assets/js/main.js
@@ -0,0 +1,9 @@
+const courierFormEl = document.getElementById("courierForm");
+const formSubeteBtnEl = document.getElementById("formSubeteBtn")
+
+formSubeteBtnEl.addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log("correct working")
+});
+
+
